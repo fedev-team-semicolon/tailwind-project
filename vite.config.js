0f765ea4d127ff23b7bdb0f5d0fed1a6c7d76f08
sohyerim-dev <fnmnn1@gmail.com>
2025-09-19 10:40:42 +0900

    tailwind setting

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import path from "path";
-import fs from "fs";
+import { defineConfig } from 'vite';
+import path from 'path';
+import fs from 'fs';
+import tailwindcss from '@tailwindcss/vite';
 
 // findAllHtmlFiles 함수 정의 추가
 function findAllHtmlFiles(directory) {
@@ -15,9 +16,9 @@ function findAllHtmlFiles(directory) {
 
       if (stat.isDirectory()) {
         scanDirectory(filePath);
-      } else if (file.endsWith(".html")) {
+      } else if (file.endsWith('.html')) {
         // 키 이름을 경로에서 추출 (확장자 제외)
-        const key = path.relative(__dirname, filePath).replace(".html", "");
+        const key = path.relative(__dirname, filePath).replace('.html', '');
         htmlFiles[key] = filePath;
       }
     }
@@ -28,15 +29,16 @@ function findAllHtmlFiles(directory) {
 }
 
 export default defineConfig({
+  plugins: [tailwindcss()],
   build: {
     rollupOptions: {
       input: {
-        index: path.resolve(__dirname, "index.html"),
-        ...findAllHtmlFiles(path.resolve(__dirname, "src")),
+        index: path.resolve(__dirname, 'index.html'),
+        ...findAllHtmlFiles(path.resolve(__dirname, 'src')),
       },
     },
   },
-  appType: "mpa", // fallback 사용안함
+  appType: 'mpa', // fallback 사용안함
   server: {
     // open: 'src/pages/main/index.html', // 서버 시작 시 브라우저에서 지정페이지 자동으로 열기
   },
